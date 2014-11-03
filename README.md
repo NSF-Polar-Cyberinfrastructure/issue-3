ea1cdfff2897a2a2f29ed7b2ec2ab7b10d154292
Tyler Palsulich <tpalsulich@gmail.com>
2014-11-03 18:06:02 -0500

    Update parse script to parse all of the data readings into the JSON.

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ issue-3
  Antarctic Meteorology Research Center (AMRC) datasets
 
 
-Crawl the station names and locations into a JSON file by running `./parse.sh [directory name]`.
-This will `wget` data files and parse them into a single JSON file sent to std out.
+Crawl the station names, locations, and data collected into a JSON file by running 
+`./parse.sh [directory name]`.
+This will `wget` data files from the AMRC FTP server and parse them into a single (large!) JSON 
+file sent to standard out. If the directory given is not empty, it will be assumed to contain 
+_only_ the crawled data files.
--- parse.sh
+++ parse.sh
@@ -2,21 +2,43 @@
 
 mkdir -p $1
 cd $1
-rm -rf ./*
-wget -q -nd -r ftp://amrc.ssec.wisc.edu/pub/requests/DVPC/AWS_q3h_MAY_2014/\*
+[ ! "$(ls -A ./)" ] && wget -q -nd -r ftp://amrc.ssec.wisc.edu/pub/requests/DVPC/AWS_q3h_MAY_2014/\*
 cd ..
 
 echo "["
-find $1 -exec awk '
+
+find $1 -type f -exec awk '
+BEGIN { printf "{" }
 {
 	if (NR==1) 
-		printf "\t{\"name\":\"" $10 "\", "; 
-	if (NR==2) 
-		print "\"lat\": \"" $2 "\", \"lon\": \"" $4 "\", \"elev\": \"" $6 "\"}"
-}' '{}' ';' | \
-awk -v NLINES="$(ls data | wc -l)" '{
+		printf "\"name\":\"" $10 "\", "; 
+	else if (NR==2) {
+		printf "\"lat\": \"" $2 "\""
+		printf ", \"lon\": \"" $4 "\""
+		printf ", \"elev\": \"" $6 "\""
+		printf ", \"data\": ["
+	}
+	else {
+		if (NR > 3) printf ", "
+		printf "{"
+		printf "\"year\": " $1 ", "
+		printf "\"julian day\": " $2 ", "
+		printf "\"month\": " $3 ", "
+		printf "\"day\": " $4 ", "
+		printf "\"ten minute obs time\": " $5 ", "
+		printf "\"temp\": " $6 ", "
+		printf "\"pressure\": " $7 ", "
+		printf "\"wind speed\": " $8 ", "
+		printf "\"wind direction\": " $9 ", "
+		printf "\"humidity\": " $10 ", "
+		printf "\"delta-T\": " $11
+		printf "}"
+	}
+}
+END {print "]}"}' '{}' ';' | \
+awk '{
+	if (NR > 1) print ", "
 	printf $0;
-	if (NR < NLINES) print ",";
-	else print " "
 }'
+echo
 echo "]"
