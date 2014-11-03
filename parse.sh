b2e6860c4103f80ec2a38c9772c89d9ad9cc3841
Tyler Palsulich <tpalsulich@gmail.com>
2014-11-03 18:31:54 -0500

    Don't show redundant or unpopulated fields.

--- parse.sh
+++ parse.sh
@@ -10,28 +10,30 @@ echo "["
 find $1 -type f -exec awk '
 BEGIN { printf "{" }
 {
-	if (NR==1) 
-		printf "\"name\":\"" $10 "\", "; 
+	if (NR==1) {
+		printf "\"year\": " $2 ", "
+		printf "\"month\": " $4 ", "
+		printf "\"name\": \"" $10 "\", "
+		printf "\"id\": \"" $6 "\", "
+		printf "\"argos\": " $8 ", "
+	}
 	else if (NR==2) {
-		printf "\"lat\": \"" $2 "\""
-		printf ", \"lon\": \"" $4 "\""
-		printf ", \"elev\": \"" $6 "\""
-		printf ", \"data\": ["
+		printf "\"lat\": \"" $2 "\"" ", "
+		printf "\"lon\": \"" $4 "\"" ", "
+		printf "\"elev\": \"" $6 "\"" ", "
+		printf "\"data\": ["
 	}
 	else {
 		if (NR > 3) printf ", "
 		printf "{"
-		printf "\"year\": " $1 ", "
 		printf "\"julian day\": " $2 ", "
-		printf "\"month\": " $3 ", "
 		printf "\"day\": " $4 ", "
 		printf "\"ten minute obs time\": " $5 ", "
 		printf "\"temp\": " $6 ", "
 		printf "\"pressure\": " $7 ", "
 		printf "\"wind speed\": " $8 ", "
 		printf "\"wind direction\": " $9 ", "
-		printf "\"humidity\": " $10 ", "
-		printf "\"delta-T\": " $11
+		printf "\"humidity\": " $10
 		printf "}"
 	}
 }
